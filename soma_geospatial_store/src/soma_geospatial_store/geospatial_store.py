fb80e10bd3d2789c4e98bedf5f7d078274d140da
Paul Duckworth <scpd@leeds.ac.uk>
2015-03-20 19:54:57 +0000

    added exception onto trajectory_roi query

--- soma_geospatial_store/src/soma_geospatial_store/geospatial_store.py
+++ soma_geospatial_store/src/soma_geospatial_store/geospatial_store.py
@@ -112,8 +112,13 @@ class GeoSpatialStoreProxy():
                              "coordinates" : geom['coordinates']}}}
                 }
         
-        res = self.find_projection(query, {"soma_roi_id": 1})     #trajectory roi
-        return res[0]['soma_roi_id']      
+        res = self.find_projection(query, {"soma_roi_id": 1})
+        try:
+            ret = res[0]['soma_roi_id']
+        except:
+            print "ERROR: no ROI found"
+            ret = None
+        return ret  
 
 
     def roi_ids(self, soma_map, soma_config):
