c24b3e797f585b5106a60b39c1b0c4c4a987281a
hkaraoguz <hkaraoguz@yahoo.com.tr>
2016-03-30 15:24:04 +0200

    Latest soma2 is merged into soma fork named as soma

---
+++ soma_manager/src/data_manager/data_manager.py
@@ -0,0 +1,204 @@
+#!/usr/bin/env python
+
+import roslib; roslib.load_manifest("soma_manager")
+import rospy
+from rospkg import RosPack
+import json
+import argparse
+import random
+import copy
+import sys
+import datetime
+import time
+import math
+
+from geometry_msgs.msg import Pose
+from geometry_msgs.msg import PoseArray
+from threading import Timer
+from mongodb_store.message_store import MessageStoreProxy
+from soma2_msgs.msg import SOMA2ROIObject, SOMA2Object
+from soma_manager.srv import *
+from soma_map_manager.srv import *
+from std_msgs.msg import String
+
+
+# Soma2 Data Manager For storing and deleting data
+class SOMADataManager():
+
+    def __init__(self, db_name="soma2data", collection_name="soma2"):
+
+       # self.soma_map_name = soma_map_name
+        self._db_name = db_name
+        self._collection_name = collection_name
+
+        # Get the map information from soma2 map_manager
+        resp = self._init_map()
+        #print resp
+        self.soma_map_name = resp.map_name
+        self.map_unique_id = resp.map_unique_id
+        print "Map name: ",self.soma_map_name," Unique ID ",self.map_unique_id
+
+        # Initialize the mongodb proxy
+        self._message_store = MessageStoreProxy(database=db_name, collection=collection_name)
+
+        # Object insertion service
+        inss = rospy.Service('soma2/insert_objects', SOMA2InsertObjs, self.handle_insert_request)
+
+        # Object deletion service
+        dels = rospy.Service('soma2/delete_objects',SOMA2DeleteObjs, self.handle_delete_request)
+
+        upts = rospy.Service('soma2/update_object',SOMA2UpdateObject,self.handle_update_request)
+
+        rospy.spin()
+
+    # Listens the map information from soma2 map_manager
+    def _init_map(self):
+        print "Waiting for the map info from soma_map_manager"
+        try:
+            rospy.wait_for_service('soma2/map_info')
+            print "Map info received..."
+        except:
+           # print("No 'static_map' service")
+            return None
+        try:
+           map_info = rospy.ServiceProxy('soma2/map_info',MapInfo)
+           resp1 = map_info(0)
+           return resp1
+        except rospy.ServiceException, e:
+           print "Service call failed: %s"%e
+           return None
+
+    # Handles the soma2 objects to be inserted
+    def handle_insert_request(self,req):
+        _ids = list()
+        for obj in req.objects:
+
+          if(obj.logtimestamp == 0):
+            obj.logtimestamp = rospy.Time.now().secs
+
+          d = datetime.datetime.utcfromtimestamp(obj.logtimestamp)
+          obj.loghour = d.hour
+          obj.logminute = d.minute
+          obj.logday = d.isoweekday()
+          obj.logtimeminutes = obj.loghour*60 + obj.logminute
+
+          if (obj.header.frame_id == ""):
+              obj.header.frame_id = "/map"
+
+          if(obj.cloud.header.frame_id == ""):
+              obj.cloud.header.frame_id = "/map"
+
+          obj.map_name = self.soma_map_name
+          obj.map_unique_id = self.map_unique_id
+
+          # SOMA2 Objects are represented as a 3D point in the world so this could be set here as point
+          obj.geotype = "Point"
+
+
+          res = self.coords_to_lnglat(obj.pose.position.x,obj.pose.position.y)
+
+          geopose = Pose()
+
+          geopose.position.x = res[0]
+          geopose.position.y = res[1]
+
+          obj.geoposearray.poses.append(geopose)
+
+          try:
+                _id = self._message_store.insert(obj)
+                st = String
+                st.data = _id
+                _ids.append(st)
+
+          except:
+                return SOMA2InsertObjsResponse(False,_ids)
+
+        return SOMA2InsertObjsResponse(True,_ids)
+
+
+    # Handles the delete request of soma2 objs
+    def handle_delete_request(self,req):
+
+        for oid in req.ids:
+            res = self._message_store.query(SOMA2Object._type,message_query={"id": oid})
+            #print len(res)
+            for o,om in res:
+                try:
+                    self._message_store.delete(str(om['_id']))
+                except:
+                      return SOMA2DeleteObjsResponse(False)
+
+        return SOMA2DeleteObjsResponse(True)
+
+    # Handles the soma2 objects to be inserted
+    def handle_update_request(self,req):
+
+        obj = req.object
+
+
+        if(obj.logtimestamp == 0):
+            obj.logtimestamp = rospy.Time.now().secs
+
+        d = datetime.datetime.utcfromtimestamp(obj.logtimestamp)
+        obj.loghour = d.hour
+        obj.logminute = d.minute
+        obj.logday = d.isoweekday()
+        obj.logtimeminutes = obj.loghour*60 + obj.logminute
+
+        if (obj.header.frame_id == ""):
+            obj.header.frame_id = "/map"
+
+        if(obj.cloud.header.frame_id == ""):
+            obj.cloud.header.frame_id = "/map"
+
+        obj.map_name = self.soma_map_name
+        obj.map_unique_id = self.map_unique_id
+
+        # SOMA2 Objects are represented as a 3D point in the world so this could be set here as point
+        obj.geotype = "Point"
+
+
+        res = self.coords_to_lnglat(obj.pose.position.x,obj.pose.position.y)
+
+        geopose = Pose()
+
+        geopose.position.x = res[0]
+        geopose.position.y = res[1]
+
+        obj.geoposearray.poses.append(geopose)
+
+        try:
+            self._message_store.update_id(req.db_id, obj)
+        except:
+            return SOMA2UpdateObjectResponse(False)
+
+        return SOMA2UpdateObjectResponse(True)
+
+    def coords_to_lnglat(self, x, y):
+        earth_radius = 6371000.0 # in meters
+        lng = 90 - math.degrees(math.acos(float(x) / earth_radius))
+        lat = 90 - math.degrees(math.acos(float(y) / earth_radius))
+        return [lng , lat]
+
+
+if __name__=="__main__":
+
+    parser = argparse.ArgumentParser(prog='data_manager.py')
+#parser.add_argument("map", nargs=1, help='Path of the used 2D map')
+#parser.add_argument("map_name",nargs=1, help='Name of the used 2D map')
+    parser.add_argument("db_name", nargs='?', help='Name of the database')
+    parser.add_argument('collection_name', nargs='?', help='Name of the collection')
+
+    args = parser.parse_args(rospy.myargv(argv=sys.argv)[1:])
+
+    rospy.init_node("soma_data_manager")
+    if args.db_name is not None:
+	if args.collection_name is not None:
+           rospy.loginfo("Running SOMA data manager (dbname: %s, collection_name: %s)", args.db_name, args.collection_name)
+           SOMADataManager(args.db_name,args.collection_name)
+	else:
+	   rospy.loginfo("Running SOMA data manager (dbname: %s, collection_name: soma2)", args.db_name)
+           SOMADataManager(args.db_name)
+    else:
+        rospy.loginfo("Running SOMA data manager (dbname: soma2data, collection_name: soma2)")
+        SOMADataManager()
---
+++ soma_map_manager/src/soma_map_manager/soma_map.py
@@ -0,0 +1,183 @@
+#!/usr/bin/env python
+
+import roslib; roslib.load_manifest("soma_map_manager")
+import rospy
+from rospkg import RosPack
+import json
+import argparse
+import random
+import copy
+import sys
+
+from threading import Timer
+
+from mongodb_store.message_store import MessageStoreProxy
+from geometry_msgs.msg import Pose
+from geometry_msgs.msg import Point
+from geometry_msgs.msg import Polygon
+from geometry_msgs.msg import PoseArray
+from nav_msgs.msg import OccupancyGrid
+from nav_msgs.srv import GetMap
+
+from soma2_msgs.msg import SOMA2ROIObject
+from soma2_msgs.msg import SOMA2OccupancyMap
+from bson.objectid import ObjectId
+
+from soma_map_manager.srv import *
+
+class SOMA2MapManager():
+
+    def __init__(self):
+
+       # self.soma_map_name = soma_map_name
+        self.map_unique_id = -1
+        self.soma2map = SOMA2OccupancyMap()
+        rospy.init_node('soma_map_manager')
+        self.pub = rospy.Publisher('soma2/map', OccupancyGrid, queue_size=1)
+       
+        self._map_store=MessageStoreProxy(database="maps", collection="soma2")
+
+        if(self._init_map()):
+            s = rospy.Service('soma2/map_info', MapInfo, self.handle_map_info)
+            self.run_node()
+        print "Quitting..."
+        
+        #rospy.spin()
+    def handle_map_info(self,req):
+        return MapInfoResponse(self.soma2map.mapname,str(self.map_unique_id))
+        
+    def _init_map(self):
+        #check for the map with map name
+        res = self._map_store.query(SOMA2OccupancyMap._type, message_query={})
+        #if map is already stored just get the unique identifier    
+        if res:
+            print "These maps are found in DB, enter the number to choose: "
+            count  = 1
+            for amap,meta in res:
+                print count," ",amap.mapname
+                count +=1
+            while 1:    
+                var = raw_input("Please enter your choice (0 to load new map via an active map_server node): ")
+                print "you entered", var
+                if(var.isdigit()):
+                  if(int(var) <= len(res)):
+                    if(int(var) == 0):
+                       return self.listen_and_store_map()
+                        
+                
+                    self.soma2map,meta = res[(int(var)-1)]
+                    #print meta
+                    self.soma2map.map.data = self.rldecode(self.soma2map.rle_values,self.soma2map.rle_counts)
+                    self.map_unique_id = meta['_id']
+                    return True
+            #print self.selected_map.map.data
+            #print selected_map.map.data
+           
+            #pub.publish(selected_map.map)
+            
+        else:
+             print "No maps are found in DB. Listening map_server for a map..."
+             if(self.listen_and_store_map()):
+                return True
+             return False
+             
+    def listen_and_store_map(self):
+         occmap = SOMA2OccupancyMap() 
+         map = self._get_occupancy_map()
+         if map:
+            var = raw_input("Map is received. Please enter a name for storing it in the database: ")
+            print "you entered", var
+            occmap.map = map.map
+            occmap.mapname = var
+            tempmap = copy.deepcopy(occmap)
+            #print map.map.data[0]
+            values,counts = self.rlencode(tempmap.map.data)
+            #print values
+            #print counts
+            occmap.rle_values = values
+            #print sum(counts)
+            occmap.rle_counts = counts
+            #print len(occmap.map.data)
+            occmap.map.data = []
+            _id = self._map_store.insert(occmap)
+            self.soma2map = tempmap
+            self.map_unique_id = _id
+            return True
+         else:
+            print "map_server is not working. Please check map_server node and ensure that it is working"
+            return False
+    
+    def run_node(self):
+           rate = rospy.Rate(10) # 10hz
+           print "map is now being published on soma2/map topic"
+           while not rospy.is_shutdown():
+                self.pub.publish(self.soma2map.map)
+                rate.sleep()
+    def rlencode(self,src):
+        result = []
+        counts = []
+        values = []
+        if src:
+            current = src[0]
+            #print current
+            counter = 0
+            for e in src:
+                #print e
+                if e == current:
+                    counter += 1
+                else:
+                    counts.append(counter)
+                    values.append(current)
+                    result.append((counter, current))
+                    current = e
+                    counter = 1
+            counts.append(counter)
+            values.append(current)
+            result.append((counter, current))
+        #print result
+        return values,counts
+ 
+ 
+    def rldecode(self,values,counts):
+        q = []
+        count = 0
+        for avalue in values:
+            for i in range(counts[count]):
+                q.append(avalue)
+            count +=1
+        
+        return q
+
+    
+            
+    def _get_occupancy_map(self):
+        try:
+            rospy.wait_for_service('/static_map')
+        except:
+            print("No 'static_map' service")
+            return None
+        try:
+           static_map = rospy.ServiceProxy('/static_map',GetMap)
+           resp1 = static_map()
+           return resp1
+        except rospy.ServiceException, e:
+           print "Service call failed: %s"%e
+           return None
+
+    
+    
+        
+
+if __name__=="__main__":
+
+    # TODO: add list command
+ 
+
+  
+    
+    rospy.init_node("soma_map_manager")
+    rospy.loginfo("Running SOMA2 Map Manager")
+    SOMA2MapManager()
+    
+
+
